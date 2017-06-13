da7a66cabdb140339f7f18e3108dff7931c002e8
曹书润 <a936381813@163.com>
2017-06-13 21:24:55 +0800

    Initial Commit

---
+++ GCDTimer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GCDTimer
+//
+//  Created by student on 2017/6/13.
+//  Copyright © 2017年 LeoAiolia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GCDTimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GCDTimer
+//
+//  Created by student on 2017/6/13.
+//  Copyright © 2017年 LeoAiolia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
